94817c2dc559517282caa92b6f12f49fb1879e3b
Feose Chiki <somto.chiki@gmail.com>
2025-11-04 17:23:14 +0100

    Included cors module

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+
 app.use(express.json());
+
+app.use(cors('*'));
 
 app.listen(PORT, () =>{
     console.log(`App is running on ${PORT}`);
